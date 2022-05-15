646b53eca522daff4699bf293470844ae7ecd8fc
danielVFS <danielv759153@gmail.com>
2022-05-15 10:15:50 -0300

    feat(nextAuth): facebook login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,27 @@
 import { FaFacebook } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 import styles from './signInButton.module.scss';
 
 export function SignInButton(): JSX.Element {
-  const session = false;
+  const { data: session } = useSession();
 
   return session ? (
-    <button type="button" className={`${styles.signInButton} ${styles.logged}`}>
+    <button
+      type="button"
+      className={`${styles.signInButton} ${styles.logged}`}
+      onClick={() => signOut()}
+    >
       <FaFacebook color="#11b85c" />
-      Daniel Vitor
+      {session.user.name ?? 'Usuário'}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={`${styles.signInButton} ${styles.notLogged}`}
+      onClick={() => signIn('facebook')}
     >
       <FaFacebook color="#dd9e20" /> Login com Facebook
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { AppProps } from 'next/app';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 import Head from 'next/head';
 
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Head>
         <title>SpaceTraveling</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
