5093476bc8d4af20247374f5ff2255a6bec63088
danielVFS <danielv759153@gmail.com>
2022-05-15 17:53:27 -0300

    styles(comment): styling the component

--- src/Comments/index.tsx
+++ src/Comments/index.tsx
@@ -1,5 +1,7 @@
-import { SyntheticEvent } from 'react';
+import { useSession } from 'next-auth/react';
+import { SyntheticEvent, useState } from 'react';
 import { FiX } from 'react-icons/fi';
+
 import styles from './comments.module.scss';
 
 interface CommentsProps {
@@ -7,15 +9,44 @@ interface CommentsProps {
 }
 
 export function Comments({ closeComments }: CommentsProps): JSX.Element {
-  return (
-    <div className={styles.container}>
-      <header>
-        <h3>Comentários</h3>
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState({});
 
-        <button type="button" onClick={closeComments}>
-          <FiX />
-        </button>
-      </header>
+  const { data: session } = useSession();
+
+  return (
+    <div className={styles.commentsContainer}>
+      <section>
+        <header>
+          <h3>Comentários (0)</h3>
+          <button type="button" onClick={closeComments}>
+            <FiX />
+          </button>
+        </header>
+        <main>
+          <div className={styles.user}>
+            <img src={session.user.image} alt={session.user.name} />
+            <p>{session.user.name}</p>
+          </div>
+          <textarea
+            placeholder="Faça um comentário"
+            onChange={e => setComment(e.target.value)}
+          />
+
+          <div className={styles.buttons}>
+            <button type="button" className={styles.cancelButton}>
+              Cancelar
+            </button>
+            <button type="button" className={styles.commentButton}>
+              Comentar
+            </button>
+          </div>
+
+          <div className={styles.comments}>
+            {comments && <p>Seja o primeiro a comentar.</p>}
+          </div>
+        </main>
+      </section>
     </div>
   );
 }
